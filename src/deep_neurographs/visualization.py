dd860084cf0f32b7be4f4f0e3920b3a551f5d0d2
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2024-02-08 14:33:30 -0800

    Reconfigure edges (#52)
    * refactor: remove immutable edge notion, upd coord conversions
    
    * refactor: mutable_edges-->proposals and, removed from graph edges
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/deep_neurographs/visualization.py
+++ src/deep_neurographs/visualization.py
@@ -44,28 +44,45 @@ def visualize_connected_components(
         plot(data, title)
 
 
-def visualize_immutables(graph, title="Initial Segmentation"):
-    data = plot_edges(graph, graph.get_immutables_temp())
+def visualize_graph(graph, title="Initial Segmentation"):
+    data = plot_edges(graph, graph.edges)
     data.append(plot_nodes(graph))
     plot(data, title)
 
 
 def visualize_proposals(graph, title="Edge Proposals"):
-    visualize_subset(graph, graph.mutable_edges, title=title)
+    visualize_subset(graph, graph.proposals, proposal_subset=True, title=title)
 
 
 def visualize_targets(graph, target_graph=None, title="Target Edges"):
     visualize_subset(
-        graph, graph.target_edges, target_graph=target_graph, title=title
+        graph,
+        graph.target_edges,
+        proposal_subset=True,
+        target_graph=target_graph,
+        title=title,
     )
 
 
-def visualize_subset(graph, edges, line_width=5, target_graph=None, title=""):
-    data = plot_edges(graph, graph.get_immutables_temp(), color="black")
-    data.extend(plot_edges(graph, edges, line_width=line_width))
+def visualize_subset(
+    graph,
+    edges,
+    line_width=5,
+    proposal_subset=False,
+    target_graph=None,
+    title=""
+):
+    # Plot graph
+    data = plot_edges(graph, graph.edges, color="black")
     data.append(plot_nodes(graph))
+    if proposal_subset:
+       data.extend(plot_proposals(graph, edges, line_width=line_width))
+    else:
+        data.extend(plot_edges(graph, edges, line_width=line_width))
+
+    # Add target graph (if applicable)
     if target_graph:
-        edges = target_graph.get_immutables_temp()
+        edges = target_graph.edges
         data.extend(plot_edges(target_graph, edges, color="blue"))
     plot(data, title)
 
@@ -82,6 +99,24 @@ def plot_nodes(graph):
         name="Nodes",
         marker=dict(size=3, color="red"),
     )
+
+
+def plot_proposals(graph, proposals, color=None, line_width=3.5):
+    traces = []
+    line = (
+        dict(width=5) if color is None else dict(color=color, width=line_width)
+    )
+    for edge in proposals:
+        trace = go.Scatter3d(
+            x=graph.proposals[edge]["xyz"][:, 0],
+            y=graph.proposals[edge]["xyz"][:, 1],
+            z=graph.proposals[edge]["xyz"][:, 2],
+            mode="lines",
+            line=line,
+            name="{}".format(edge),
+        )
+        traces.append(trace)
+    return traces
 
 
 def plot_edges(graph, edges, color=None, line_width=3.5):
