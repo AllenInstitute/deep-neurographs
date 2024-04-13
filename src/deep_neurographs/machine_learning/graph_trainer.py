d1f33ff1bd165bbdfa2eae509864523ea647d10b
anna-grim <anna.grim@alleninstitute.org>
2024-04-13 02:15:42 +0000

    feat: logging and track best

--- src/deep_neurographs/machine_learning/graph_trainer.py
+++ src/deep_neurographs/machine_learning/graph_trainer.py
@@ -9,9 +9,13 @@ Routines for training graph neural networks that classify edge proposals.
 """
 
 from random import sample, shuffle
-
+import numpy as np
 import torch
+from copy import deepcopy
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from torch.nn.functional import sigmoid
+from torch.utils.tensorboard import SummaryWriter
+
 
 LR = 1e-3
 N_EPOCHS = 300
@@ -19,71 +23,90 @@ TEST_PERCENT = 0.15
 WEIGHT_DECAY = 5e-4
 
 
-def run_on_graph(model, graph_data):
-    pass
+class GraphTrainer:
+    def __init__(
+        self,
+        model,
+        criterion,
+        lr=LR,
+        n_epochs=N_EPOCHS,
+        weight_decay=WEIGHT_DECAY,
+    ):
+        self.model = model.to("cuda:0")
+        self.n_epochs = n_epochs
+        self.optimizer = torch.optim.Adam(
+            model.parameters(), lr=lr, weight_decay=weight_decay
+        )
+        self.writer = SummaryWriter()
 
+    def run_on_graphs(self, graph_datasets):
+        # Initializations
+        best_score = -np.inf
+        best_ckpt = None
 
-def run_on_graphs(
-    model,
-    criterion,
-    graph_datasets,
-    lr=LR,
-    n_epochs=N_EPOCHS,
-    weight_decay=WEIGHT_DECAY,
-):
-    # Initializations
-    model.to("cuda:0")
-    optimizer = torch.optim.Adam(
-        model.parameters(), lr=lr, weight_decay=weight_decay
-    )
+        # Main
+        train_ids, test_ids = train_test_split(list(graph_datasets.keys()))
+        for epoch in range(self.n_epochs):
+            # Train
+            y, hat_y = [], []
+            self.model.train()
+            for graph_id in train_ids:
+                y_i, hat_y_i = self.forward(graph_datasets[graph_id].data)
+                y.extend(toCPU(y_i))
+                hat_y.extend(toCPU(hat_y_i))
+            train_score = self.compute_metrics(y, hat_y, "train", epoch)
 
-    # Main
-    accuracy = []
-    train_ids, test_ids = train_test_split(list(graph_datasets.keys()))
-    for epoch in range(n_epochs):
-        # Train
-        model.train()
-        for graph_id in train_ids:
-            loss, optimizer = train(
-                model, criterion, optimizer, graph_datasets[graph_id].data
-            )
+            # Test
+            y, hat_y = [], []
+            self.model.eval()
+            for graph_id in test_ids:
+                y_i, hat_y_i = self.forward(graph_datasets[graph_id].data)
+                y.extend(toCPU(y_i))
+                hat_y.extend(toCPU(hat_y_i))
+            test_score = self.compute_metrics(y, hat_y, "val", epoch)
 
-        # Test
-        model.eval()
-        accuracy_i = 0
-        for graph_id in test_ids:
-            accuracy_i += validate(model, graph_datasets[graph_id].data)
-        accuracy.append(accuracy_i / len(test_ids))
-        if epoch % 10 == 0:
-            print("Accuracy +/-:", accuracy[-1])
-    return model
+            # Check for best
+            print(test_score)
+            if test_score > best_score:
+                best_score = test_score
+                best_ckpt = deepcopy(self.model.state_dict())
+        return self.model.load_state_dict(best_ckpt)
 
+    def train(self, graph_data):
+        y, hat_y = self.forward_pass(graph_data)
+        self.backpropagate(y, hat_y)
+        return y, hat_y
 
-def train(model, criterion, optimizer, graph_data):
-    # Forward pass
-    x, y, edge_index = toGPU(graph_data)
-    optimizer.zero_grad()
-    hat_y = model(x, edge_index)
-    hat_y = truncate(hat_y, y)
+    def forward(self, graph_data):
+        self.optimizer.zero_grad()
+        x, y, edge_index = toGPU(graph_data)
+        hat_y = self.model(x, edge_index)
+        return y, truncate(hat_y, y)
 
-    # Backward pass
-    loss = criterion(hat_y, y)
-    loss.backward()
-    optimizer.step()
-    return loss, optimizer
+    def backpropagate(self, y, hat_y):
+        loss = self.criterion(hat_y, y)
+        loss.backward()
+        self.optimizer.step()
 
+    def compute_metrics(self, y, hat_y, prefix, epoch):
+        # Initializations
+        y = np.array(y, dtype=int).tolist()
+        hat_y = get_predictions(hat_y)
 
-def validate(model, graph_data):
-    # Initializations
-    x, y, edge_index = toGPU(graph_data)
-    hat_y = model(x, edge_index)
-    hat_y = truncate(hat_y, y)
+        # Compute
+        accuracy = accuracy_score(y, hat_y)
+        accuracy_dif = accuracy - np.sum(y) / len(y)
+        precision = precision_score(y, hat_y)
+        recall = recall_score(y, hat_y)
+        f1 = f1_score(y, hat_y)
 
-    # Compute accuracy
-    preds = get_predictions(hat_y)
-    correct = preds == y
-    acc = float(correct.sum()) / y.size(0)
-    return acc - y.sum() / y.size(0)
+        # Log
+        self.writer.add_scalar(prefix + '_accuracy:', accuracy, epoch)
+        self.writer.add_scalar(prefix + '_accuracy_df:', accuracy_dif, epoch)
+        self.writer.add_scalar(prefix + '_precision:', precision, epoch)
+        self.writer.add_scalar(prefix + '_recall:', recall, epoch)
+        self.writer.add_scalar(prefix + 'val_f1:', f1, epoch)
+        return accuracy_dif
 
 
 # -- utils --
@@ -111,6 +134,10 @@ def train_test_split(graph_ids):
     test_ids = sample(graph_ids, n_test_examples)
     train_ids = list(set(graph_ids) - set(test_ids))
     return train_ids, test_ids
+
+
+def toCPU(tensor):
+    return np.array(tensor.detach().cpu()).tolist()
 
 
 def toGPU(graph_data):
@@ -143,4 +170,8 @@ def truncate(hat_y, y):
 
 
 def get_predictions(hat_y, threshold=0.5):
-    return sigmoid(hat_y) > threshold
+    return (sigmoid(np.array(hat_y)) > threshold).tolist()
+
+
+def sigmoid(x):
+    return 1.0/(1.0 + np.exp(-x))
