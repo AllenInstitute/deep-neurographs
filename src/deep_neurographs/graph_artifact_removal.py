028321ee99ae2f11734fae2a6ae954449df08c60
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2024-07-07 10:43:20 -0700

    Feat trim passes (#193)
    * feat: initial trim
    
    * feat: upds to trim passing
    
    * refactor: v0 trim passes
    
    * bug: trim passes
    
    * bug: save merged proposals
    
    * refactor: improved doubles detection:
    
    * feat: trim endpoints in proposal generation
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/deep_neurographs/graph_artifact_removal.py
+++ src/deep_neurographs/graph_artifact_removal.py
@@ -14,9 +14,8 @@ from time import time
 import networkx as nx
 import numpy as np
 
-from deep_neurographs import graph_utils as gutils
 from deep_neurographs import utils
-from deep_neurographs.geometry import dist, path_length
+from deep_neurographs.geometry import dist
 
 COLOR = "1.0 0.0 0.0"
 MAX_DEPTH = 16
@@ -63,7 +62,7 @@ def remove_doubles(neurograph, max_size, node_spacing, output_dir=None):
                 # Check doubles criteria
                 n_points = len(neurograph.edges[tuple(nodes[idx])]["xyz"])
                 hits = compute_hits(neurograph, tuple(nodes[idx]), swc_id)
-                if check_doubles_criteria(hits, n_points, swc_id):
+                if check_doubles_criteria(hits, n_points):
                     if output_dir:
                         neurograph.to_swc(output_dir, nodes[idx], color=COLOR)
                     neurograph = delete_nodes(neurograph, nodes[idx], swc_id)
@@ -104,53 +103,30 @@ def compute_hits(neurograph, edge, query_id):
     return hits
 
 
-def check_doubles_criteria(hits, n_points, swc_id):
+def check_doubles_criteria(hits, n_points):
     """
     Determines whether the connected component corresponding to "root" is a
     double of another connected component.
 
     Paramters
     ---------
-    neurograph : NeuroGraph
-        Graph to be searched for doubles.
-    fragment : numpy.ndarray
-        Array containing xyz coordinates corresponding to some fragment (i.e.
-        connected component in neurograph).
-    swc_id_i : str
-        swc id corresponding to fragment.
+    hits : dict
+        ...
+    n_points : int
+        Number of nodes that comprise the component being checked.
 
     Returns
     -------
-    str or None
-        Indication of whether connected component is a double. If True, the
-        swc_id of the main fragment (i.e. non doubles) is returned. Otherwise,
-        the value None is returned to indicate that query fragment is not a
-        double.
+    bool
+        Indication of whether component is a double.
 
     """
-    for hit_id, dists in hits.items():
+    for dists in hits.values():
         if len(dists) > 5:
-            percent_hit = len(dists) / n_points
-            dist_std = np.std(dists)
-            if percent_hit > 0.5 and dist_std < 2:
+            if len(dists) / n_points > 0.5 and np.std(dists) < 2:
                 return True
-            elif percent_hit > 0.75 and dist_std < 2.5:
+            elif len(dists) / n_points > 0.75 and np.std(dists) < 2.5:
                 return True
-            """
-            elif len(dists) > 10:
-                dists = np.array(dists)
-                if np.mean(dists[0:10]) < 5:
-                    print(swc_id, hit_id)
-                    print(percent_hit, dist_std)
-                    print(
-                        len(dists),
-                        np.sum(dists < 5),
-                        np.mean(dists),
-                        np.mean(dists[0:10]),
-                        np.sum(dists < 5) / n_points
-                    )
-                    print("")
-            """
     return False
 
 
@@ -236,188 +212,3 @@ def upd_hits(hits, key, value):
     else:
         hits[key] = value
     return hits
-
-
-# --- Trim Passings ---
-def trim_passings(neurograph):
-    # Initializations
-    cnt = 1
-    t0, t1 = utils.init_timers()
-    chunk_size = int(len(neurograph.leafs) * 0.02)
-
-    # Main
-    n_endpoints_trimmed = 0
-    for leaf in neurograph.leafs:
-        if leaf in neurograph.nodes:
-            hits = search_along_branch(neurograph, leaf)
-            if len(hits) == 1:
-                neurograph = simple_trim(neurograph, leaf, hits)
-                n_endpoints_trimmed += 1
-            elif len(hits) > 1:
-                print("complex trim")
-    print("# hits:", n_endpoints_trimmed)
-
-
-def search_along_branch(neurograph, leaf):
-    hits = dict()
-    swc_id_leaf = neurograph.nodes[leaf]["swc_id"]
-    for xyz_leaf, radius in get_branch(neurograph, leaf):
-        for xyz in neurograph.query_kdtree(xyz_leaf, radius + 1):
-            try:
-                swc_id = neurograph.xyz_to_swc(xyz)
-                if swc_id != swc_id_leaf:
-                    hits = utils.append_dict_value(hits, swc_id, xyz)
-            except:
-                pass
-    return keep_passings(hits) if len(hits) > 0 else hits
-
-
-def keep_passings(hits):
-    rm_keys = list()
-    for swc_id, xyz_coords in hits.items():
-        if path_length(xyz_coords) < 5:
-            rm_keys.append(swc_id)
-    return utils.remove_items(hits, rm_keys)
-
-
-def simple_trim(neurograph, leaf, hits):
-    swc_id, xyz_coords = unpack_dict(hits)
-    i, j = get_edge(neurograph, xyz_coords)
-    if neurograph.is_leaf(i) or neurograph.is_leaf(j):
-        # Check for significant difference in radii
-        i = get_endpoint(neurograph, leaf, (i, j))
-        radius_leaf = neurograph.nodes[leaf]["radius"]
-        radius_edge = neurograph.nodes[i]["radius"]
-        
-        if radius_leaf < radius_edge - 1 or radius_leaf / radius_edge < 0.75:
-            print("Case 1:")
-            print("trimming:", neurograph.nodes[leaf]["swc_id"])
-            print("not trimming:", swc_id)
-            neurograph = trim_from_leaf(neurograph, leaf, xyz_coords)
-        elif radius_edge < radius_leaf - 1 or radius_edge / radius_leaf < 0.75:
-            print("Case 2:")
-            print("trimming:", neurograph.nodes[leaf]["swc_id"])
-            print("not trimming:", swc_id)
-            neurograph = trim_from_leaf(neurograph, i, xyz_coords)
-        else:
-            # Determine smaller fragment
-            leaf_component_size = len(gutils.get_component(neurograph, leaf))
-            edge_component_size = len(gutils.get_component(neurograph, i))
-            if leaf_component_size < edge_component_size:
-                print("Case 3a:")
-                print("trimming:", neurograph.nodes[leaf]["swc_id"])
-                print("not trimming:", swc_id)
-                neurograph = trim_from_leaf(neurograph, leaf, xyz_coords)
-            else:
-                print("Case 3b:")
-                print("trimming:", neurograph.nodes[leaf]["swc_id"])
-                print("not trimming:", swc_id)
-                neurograph = trim_from_leaf(neurograph, i, xyz_coords)
-    return neurograph
-
-
-def get_endpoint(neurograph, leaf, edge):
-    i, j = tuple(edge)
-    d_i = dist(neurograph.nodes[i]["xyz"], neurograph.nodes[leaf]["xyz"])
-    d_j = dist(neurograph.nodes[j]["xyz"], neurograph.nodes[leaf]["xyz"])
-    return i if d_i < d_j else j
-
-
-def trim_from_leaf(neurograph, leaf, xyz_coords):
-    # Initializations
-    j = list(neurograph.neighbors(leaf))[0]
-    xyz_coords = set([tuple(xyz) for xyz in xyz_coords])
-
-    # Determine points to trim
-    idx = 0
-    zipped_branch = get_branch(neurograph, leaf)
-    while len(xyz_coords) > 0:
-        for xyz_query, radius in zipped_branch:
-            for xyz in neurograph.query_kdtree(xyz_query, radius + 0.5):
-                if tuple(xyz) in xyz_coords:
-                    xyz_coords.remove(tuple(xyz))
-        idx += 1
-        if idx > len(neurograph.edges[leaf, j]["xyz"]):
-            break
-    xyz_coords = neurograph.oriented_edge((leaf, j), leaf)
-    idx = min(len(xyz_coords), idx + 2)
-
-    # Trim points
-    if path_length(xyz_coords[idx::]) > 15:
-        print(f"--> trimmed {round(path_length(xyz_coords[0:idx]), 2)} microns \n")
-        neurograph = trim(neurograph, leaf, j, xyz_coords, idx)
-    else:
-        print("--> deleting\n")
-        neurograph.remove_node(leaf)
-        if neurograph.degree[j] == 0:
-            neurograph.remove_node(j)
-    return neurograph
-
-
-def trim(neurograph, leaf, j, xyz_coords, idx):
-    e = (leaf, j)
-    neurograph.nodes[leaf]["xyz"] = xyz_coords[idx]
-    neurograph.edges[leaf, j]["xyz"] = xyz_coords[idx::]
-    neurograph.edges[e]["radius"] = neurograph.edges[e]["radius"][idx::]
-    return neurograph    
-
- 
-# --- utils ---
-def get_branch(neurograph, i):
-    """
-    Gets the xyz coordinates of the branch emanating from "i".
-
-    Parameters
-    ----------
-    neurograph : NeuroGraph
-        Graph containing node "i".
-    i : int
-        Leaf node in "neurograph".
-
-    Returns
-    -------
-    numpy.ndarray
-        xyz coordinates of branch emanating from "i".
-
-    """
-    j = list(neurograph.neighbors(i))[0]
-    xyz_coords = neurograph.oriented_edge((i, j), i)
-    radii = neurograph.oriented_edge((i, j), i, key="radius")
-    n = max(len(xyz_coords), MAX_DEPTH)
-    return zip(xyz_coords[0:n], radii[0:n])
-
-
-def get_branch_avg_radii(neurograph, leaf):
-    """
-    Gets the average radii of the branch emanating from "leaf".
-
-    Parameters
-    ----------
-    neurograph : NeuroGraph
-        Graph containing "leaf".
-    leaf : int
-        Node id of leaf node.
-
-    Returns
-    -------
-    float
-        Average radii of the branch emanating from "leaf".
-
-    """
-    j = list(neurograph.neighbors(leaf))[0]
-    return np.mean(neurograph.oriented_edge((leaf, j), leaf, key="radius"))
-
-
-def unpack_dict(my_dict):
-    return list(my_dict.items())[0]
-
-
-def get_edge(neurograph, xyz_coords):
-    hits = dict()
-    for xyz in xyz_coords:
-        try:
-            edge = neurograph.xyz_to_edge[tuple(xyz)]
-            hits = utils.append_dict_value(hits, edge, 1)
-        except:
-            pass
-    return tuple(utils.find_best(hits))
