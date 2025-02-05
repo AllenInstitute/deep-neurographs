f830b7c45f547eff14c9ea65e0531d5d3275e57f
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-02-04 18:22:23 -0800

    refactor: commented out flag due to speed

--- src/deep_neurographs/inference.py
+++ src/deep_neurographs/inference.py
@@ -579,7 +579,7 @@ class InferenceEngine:
             proposals = sort_proposals(fragments_graph, proposals)
 
         # Main
-        flagged = get_large_proposal_components(fragments_graph, 4)
+        flagged = set()  # too slow - get_large_proposal_components(fragments_graph, 4)
         with tqdm(total=len(proposals), desc="Inference") as pbar:
             accepts = list()
             while len(proposals) > 0:
@@ -593,7 +593,7 @@ class InferenceEngine:
                     fragments_graph.merge_proposal(p)
                     accepts.append(p)
                 pbar.update(len(batch["proposals"]))
-            fragments_graph.absorb_reducibles()  # - extremely slow
+            #fragments_graph.absorb_reducibles()  # - extremely slow
         return fragments_graph, accepts
 
     def get_batch(self, fragments_graph, proposals, flagged_proposals):
