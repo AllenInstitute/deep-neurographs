fca3edf681f20c2f6fb5341759ac6ce01d650533
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-03-18 18:28:00 -0700

    linting (#382)
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/deep_neurographs/utils/ml_util.py
+++ src/deep_neurographs/utils/ml_util.py
@@ -10,7 +10,6 @@ models.
 """
 
 from collections import deque
-from time import time
 
 import networkx as nx
 import numpy as np
@@ -59,7 +58,6 @@ def get_batch(graph, proposals, batch_size, flagged_proposals=set()):
         queue.append((j, 0))
 
     # Main
-    t0 = time()
     batch = {"graph": nx.Graph(), "proposals": set()}
     visited = set()
     while len(proposals) > 0 and len(batch["proposals"]) < batch_size:
