c5443fc69de0d0efeaf5475c5a7162c01d37f8d8
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-08-12 15:46:13 -0700

    Feat merge inference (#467)
    * feat: graph traversal and batch generation
    
    * refactor: threaded merge inference
    
    * feat: implemeneted traversal step size
    
    * refactor: removed test code
    
    * bug: added label mask
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/deep_neurographs/merge_proofreading/inference.py
+++ src/deep_neurographs/merge_proofreading/inference.py
@@ -31,7 +31,7 @@ import networkx as nx
 import numpy as np
 import torch
 
-from deep_neurographs.utils import img_util
+from deep_neurographs.utils import img_util, ml_util
 
 
 class MergeDetector:
@@ -55,6 +55,11 @@ class MergeDetector:
         self.remove_detected_sites = remove_detected_sites
         self.threshold = threshold
 
+        # Load model
+        self.model = model
+        ml_util.load_model(model, model_path)
+
+        # Initialize dataset
         self.dataset = IterableGraphDataset(
             graph,
             img_path,
@@ -66,15 +71,25 @@ class MergeDetector:
         )
 
     # --- Core routines
-    def search_graph(self):
+    def search_merge_sites(self):
         # Iterate over dataset
         detected_merge_sites = list()
-        for batch in self.dataset:
-            pass
+        for nodes, x_nodes in self.dataset:
+            hat_y = self.predict(x_nodes)
+            idxs = np.where(hat_y > self.threshold)[0]
+            detected_merge_sites.extend([nodes[i] for i in idxs])
+            print(hat_y)
+        print("# Detected Merge Sites:", len(detected_merge_sites))
 
         # Optionally, remove merge mistakes from graphs
         if self.remove_detected_sites:
             pass
+
+    def predict(self, x):
+        with torch.no_grad():
+            x = x.to("cuda")
+            hat_y = self.model(x)
+            return ml_util.to_cpu(hat_y, to_numpy=True)
 
     def remove_merge_sites(self, detected_merge_sites):
         pass
@@ -113,9 +128,9 @@ class IterableGraphDataset(IterableDataset):
         # Subroutines
         def submit_thread():
             try:
-                node_ids, patch_centers = next(batch_metadata_iter)
+                nodes, patch_centers = next(batch_metadata_iter)
                 thread = executor.submit(self.read_superchunk, patch_centers)
-                pending[thread] = (node_ids, patch_centers)
+                pending[thread] = (nodes, patch_centers)
             except StopIteration:
                 pass
 
@@ -133,10 +148,9 @@ class IterableGraphDataset(IterableDataset):
                     done, _ = wait(pending.keys(), return_when=FIRST_COMPLETED)
                     for thread in done:
                         # Process completed thread
-                        node_ids, patch_centers = pending.pop(thread)
-                        superchunk, offset = thread.result()
-                        patch_centers -= offset
-                        yield self.get_batch(superchunk, patch_centers, node_ids)
+                        nodes, patch_centers = pending.pop(thread)
+                        img, offset = thread.result()
+                        yield self.get_batch(img, offset, patch_centers, nodes)
 
                         # Continue submitting threads
                         submit_thread()
@@ -145,7 +159,7 @@ class IterableGraphDataset(IterableDataset):
 
     def generate_batch_metadata(self):
         """
-        Generates metadata (node_ids, patch_centers) used to generate batches.
+        Generates metadata (nodes, patch_centers) used to generate batches.
 
         Parameters
         ----------
@@ -166,7 +180,7 @@ class IterableGraphDataset(IterableDataset):
 
     def _generate_batch_metadata_for_component(self, root):
         """
-        Generates metadata (node_ids, patch_centers) used to generate batches
+        Generates metadata (nodes, patch_centers) used to generate batches
         for the connected component containing the given root node.
 
         Parameters
@@ -180,7 +194,7 @@ class IterableGraphDataset(IterableDataset):
             batches for the connected component containing the given root
             node.
         """
-        node_ids = list()
+        nodes = list()
         patch_centers = list()
         visited = set()
         for i, j in nx.dfs_edges(self.graph, source=root):
@@ -188,7 +202,7 @@ class IterableGraphDataset(IterableDataset):
             if len(patch_centers) == 0:
                 root = i
                 last_node = i
-                node_ids.append(i)
+                nodes.append(i)
                 patch_centers.append(self.get_voxel(i))
                 visited.add(i)
 
@@ -196,8 +210,8 @@ class IterableGraphDataset(IterableDataset):
             is_node_far = self.graph.dist(root, j) > 512
             is_batch_full = len(patch_centers) == self.batch_size
             if is_node_far or is_batch_full:
-                yield node_ids, np.array(patch_centers, dtype=int)
-                node_ids = list()
+                yield nodes, np.array(patch_centers, dtype=int)
+                nodes = list()
                 patch_centers = list()
 
             # Visit j
@@ -207,25 +221,27 @@ class IterableGraphDataset(IterableDataset):
                 is_branching = self.graph.degree[j] >= 3
                 if is_next or is_branching:
                     last_node = j
-                    node_ids.append(j)
+                    nodes.append(j)
                     patch_centers.append(self.get_voxel(j))
                     if len(patch_centers) == 1:
                         root = j
 
         # Yield any remaining nodes after the loop
         if patch_centers:
-            yield node_ids, np.array(patch_centers, dtype=int)
+            yield nodes, np.array(patch_centers, dtype=int)
 
-    def get_batch(self, superchunk, patch_centers, node_ids):
-        batch = np.empty((len(patch_centers), 1,) + self.patch_shape)
+    def get_batch(self, img, offset, patch_centers, nodes):
+        # Initializations
+        label_mask = self.get_label_mask(nodes, img.shape, offset)
+        patch_centers -= offset
+
+        # Populate batch array
+        batch = np.empty((len(patch_centers), 2,) + self.patch_shape)
         for i, center in enumerate(patch_centers):
             s = img_util.get_slices(center, self.patch_shape)
-            batch[i, 0, ...] = superchunk[s]
-            superchunk[tuple(center)] = 1.5
-        from tifffile import imwrite
-        imwrite("superchunk.tif", superchunk)
-        print("superchunk.shape:", superchunk.shape)
-        return node_ids, torch.tensor(batch, dtype=torch.float)
+            batch[i, 0, ...] = img[s]
+            batch[i, 1, ...] = label_mask[s]
+        return nodes, torch.tensor(batch, dtype=torch.float)
 
     # --- Helpers ---
     def read_superchunk(self, patch_centers):
@@ -239,6 +255,32 @@ class IterableGraphDataset(IterableDataset):
         center = (start + shape // 2).astype(int)
         superchunk = img_util.normalize(self.img_reader.read(center, shape))
         return superchunk, start.astype(int)
+
+    def get_label_mask(self, nodes, img_shape, offset):
+        label_mask = np.zeros(img_shape)
+        queue = list(nodes)
+        visited = set(nodes)
+        while queue:
+            # Visit node
+            node = queue.pop()
+            voxel = self.get_voxel(node) - offset
+            if img_util.is_contained(voxel, img_shape, buffer=3):
+                label_mask[
+                    max(voxel[0] - 3, 0): min(voxel[0] + 3, img_shape[0]),
+                    max(voxel[1] - 3, 0): min(voxel[1] + 3, img_shape[1]),
+                    max(voxel[2] - 3, 0): min(voxel[2] + 3, img_shape[2])
+                ] = 1
+                is_contained = True
+            else:
+                is_contained = False
+
+            # Update queue
+            if is_contained:
+                for nb in self.graph.neighbors(node):
+                    if nb not in visited:
+                        queue.append(nb)
+                        visited.add(nb)
+        return label_mask
 
     def get_voxel(self, node):
         """
